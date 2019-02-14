7e4a4a02e7b62e2d1328fa0959afd5bf9f5c6b8f
Hannif Kurniawan <hannif.kurniawan@gmail.com>
2019-02-14 23:02:46 +0700

    [NF] Create config file

--- config.php
+++ config.php
@@ -6,9 +6,7 @@ $db_pass = "password";
 $db_name = "register_project";
 
 try {    
-    //create PDO connection 
     $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
 } catch(PDOException $e) {
-    //show error
     die("Error: " . $e->getMessage());
 }
